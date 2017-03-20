3742b890803f29ec3e08be2382084b8cdea597de
Aravindasamy, Rajaprabhu (ESI) <laraprabhu@gmail.com>
2017-03-20 22:17:51 +0530

    Slider not hiding issue fixed

--- js/constants/data-constants.js
+++ js/constants/data-constants.js
@@ -3,5 +3,21 @@ const _data = {
   colCnt: 10,
 	rowCnt_showCase: 6,
   colCnt_showCase: 6,
-	loader_removal_time : 1500
+	loader_removal_time : 1500,
+	blocks: [
+		[
+			[
+				[0,1,0],
+				[0,1,0],
+				[0,1,0]
+			]
+		],
+		[
+			[
+				[0,0,0],
+				[1,1,0],
+				[1,1,0]
+			]
+		]
+	]
 };
--- js/events/implementations.js
+++ js/events/implementations.js
@@ -1,27 +1,26 @@
 "use strict";
 
 class Implementations {
-	static control_button_click() {
+	static control_button_click(){
 		_selectors.loader_spinny
 			.removeClass(_classes.remove)
 				.find(_selectors_string.spinny).addClass(_classes.hidden).end()
 					.find(_selectors_string.control_details).removeClass(_classes.hidden);
 	}
-	static got_it_button_click() {
-		_selectors.loader_spinny
-			.addClass(_classes.remove)
+	static got_it_button_click(){
+		_selectors.loader_spinny.addClass(_classes.remove);
 	}
 	static play_button_click(){
-		$(this).addClass(_classes.disabled)
-			.siblings().removeClass(_classes.disabled);
+		$(this).addClass(_classes.disabled).siblings().removeClass(_classes.disabled);
 	}
 	static pause_button_click(){
-		$(this).addClass(_classes.disabled)
-			.prev().removeClass(_classes.disabled);
+		$(this).addClass(_classes.disabled).prev().removeClass(_classes.disabled);
 	}
 	static stop_button_click(){
-		$(this).add(this.previousElementSibling)
-			.addClass(_classes.disabled).prev(_selectors_string.play_button)
-				.removeClass(_classes.disabled);
+		$(this)
+			.add(this.previousElementSibling)
+				.addClass(_classes.disabled)
+					.prev(_selectors_string.play_button)
+						.removeClass(_classes.disabled);
 	}
 }
